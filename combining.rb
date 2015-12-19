a5fc90686ec7cc0efac06138f55b507eef2bcf84
rowlandjl <rowlandjl@vcu.edu>
2015-12-18 21:08:35 -0500

    first commit

---
+++ combining.rb
@@ -0,0 +1,13 @@
+crypt_of_civilization = Array.new
+
+crypt_of_civilization << "A Comptometer"
+crypt_of_civilization << "A box of phonographic records"
+crypt_of_civilization << "A plastic savings bank"
+crypt_of_civilization << "A set of scales"
+crypt_of_civilization << "A Toast-O-Lator"
+crypt_of_civilization << "A sample of aluminum foil"
+crypt_of_civilization << "A Donald Duck doll"
+
+extra = ["container of beer", "Lionel model train set", "Ingraham pocket watch"]
+
+crypt_of_civilization = crypt_of_civilization + extra
